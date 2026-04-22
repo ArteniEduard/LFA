d491a0f9ebb0c77a81201f1c81d174932a49ad91
Eduard Arteni <eduardarteniapple@gmail.com>
2026-04-23 00:07:15 +0300

    Done

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
 import java.io.File;
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class Character{
+class Character {
     String display;
-    public Character(String display){
+
+    public Character(String display) {
         this.display = display;
     }
 
@@ -14,7 +16,7 @@ class Character{
     }
 }
 
-class Transition{
+class Transition {
     Character character;
     State state;
 
@@ -29,9 +31,9 @@ class State {
     String name;
     ArrayList<Transition> transitions;
 
-    public State(String name){
+    public State(String name) {
         this.name = name;
-        this.transitions =  new ArrayList<>();
+        this.transitions = new ArrayList<>();
         this.isFinal = false;
     }
 
@@ -40,21 +42,26 @@ class State {
         return "{" + name + ", " + isFinal + "," + transitions + "}";
     }
 }
+
 class Automaton {
     ArrayList<Character> characters;
     ArrayList<State> states;
     State initialState;
+    boolean isLambdaNFA = false;
 
-    public Automaton(String[] chars, String[] names, String initialState, String[] finalStates){
+    public Automaton(String[] chars, String[] names, String initialState, String[] finalStates, boolean isLambdaNFA) {
         characters = new ArrayList<>();
         states = new ArrayList<>();
+        if (isLambdaNFA) {
+            characters.add(new Character("lambda"));
+        }
 
-        for (String name: names) {
+        for (String name : names) {
             State state = new State(name);
-            if(initialState.equals(name)){
+            if (initialState.equals(name)) {
                 this.initialState = state;
             }
-            for (String finalStateName: finalStates) {
+            for (String finalStateName : finalStates) {
                 if (finalStateName.equals(name)) {
                     state.isFinal = true;
                     break;
@@ -62,25 +69,27 @@ class Automaton {
             }
             this.states.add(state);
         }
-        for (String character: chars) {
+        for (String character : chars) {
             Character c = new Character(character);
             characters.add(c);
         }
+
+        this.isLambdaNFA = isLambdaNFA;
     }
 
-    public void addTransition(String s){
+    public void addTransition(String s) {
         String[] split = s.split(" ");
-        for (State state: states) {
-            if(state.name.equals(split[0])){
+        for (State state : states) {
+            if (state.name.equals(split[0])) {
                 Transition transition = new Transition();
-                for (Character character:characters) {
-                    if(character.display.equals(split[1])){
+                for (Character character : characters) {
+                    if (character.display.equals(split[1])) {
                         transition.character = character;
                         break;
                     }
                 }
-                for (State state2: states) {
-                    if(state.name.equals(split[2])){
+                for (State state2 : states) {
+                    if (state2.name.equals(split[2])) {
                         transition.state = state2;
                         break;
                     }
@@ -91,36 +100,79 @@ class Automaton {
         }
     }
 
-    public boolean testWord(String word, State currentState){
-        boolean valid = true;
-        int len = 1;
-        Transition transition = null;
-        while (valid){
-            valid = false;
-            for (Transition t: currentState.transitions) {
-                if (t.character.display.equals(word.substring(0, len))) {
-                    transition = t;
-                    valid = true;
-                    len++;
-                    break;
+    public boolean testWord(String word, State currentState, boolean displaySteps) {
+        return testWord(word, currentState, displaySteps, new ArrayList<>());
+    }
+
+    public boolean testWord(String word, State currentState, boolean displaySteps, ArrayList<String> visited) {
+        String key = currentState.name + "|" + word;
+
+        if (visited.contains(key)) {
+            return false;
+        }
+
+        visited.add(key);
+
+
+        if (word.isEmpty()) {
+            if (currentState.isFinal) {
+                return true;
+            }
+
+            if (isLambdaNFA) {
+                for (Transition t : currentState.transitions) {
+                    if (t.character.display.equals("lambda")) {
+                        if (testWord(word, t.state, displaySteps, new ArrayList<>(visited))) {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        String ch = String.valueOf(word.charAt(0));
+
+        if (isLambdaNFA) {
+            for (Transition t : currentState.transitions) {
+                if (t.character.display.equals("lambda")) {
+                    if (testWord(word, t.state, displaySteps, new ArrayList<>(visited))) {
+                        return true;
+                    }
                 }
             }
         }
-        System.out.println("Finally we are using the transition " + transition);
-        return true;
+
+        for (Transition t : currentState.transitions) {
+            if (t.character.display.equals(ch)) {
+                if (displaySteps)
+                    System.out.println("Using transition: " + t);
+
+                if (testWord(word.substring(1), t.state, displaySteps, new ArrayList<>())) {
+                    return true;
+                }
+            }
+        }
+
+        return false; // no valid transitions
     }
-    public void print(){
+
+    public void print() {
         System.out.println("Characters = " + characters);
         System.out.println("States = " + states);
-        System.out.println("Initial state = " + initialState);
+        System.out.println("Initial state = " + initialState.name);
 
     }
 }
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        File input = new File("/Users/student/IdeaProjects/java243/lab2java/src/input.txt");
+        File input = new File("input.txt");
         try (Scanner scanner = new Scanner(input)) {
+
+            boolean isLambdaNFA = Boolean.parseBoolean(scanner.nextLine());
+
             String characters = scanner.nextLine();
             String[] chars = characters.split(" ");
 
@@ -133,7 +185,7 @@ public class Main{
             String finalStatesLine = scanner.nextLine();
             String[] finalStates = finalStatesLine.split(" ");
 
-            Automaton automaton = new Automaton(chars, names, initialState, finalStates);
+            Automaton automaton = new Automaton(chars, names, initialState, finalStates, isLambdaNFA);
 
             while (scanner.hasNextLine()) {
                 String transition = scanner.nextLine();
@@ -141,8 +193,35 @@ public class Main{
             }
 
             automaton.print();
-            automaton.testWord("abc", automaton.initialState);
+            //DFA
+            /*
+            System.out.println(automaton.testWord("b", automaton.initialState, false));
+            System.out.println(automaton.testWord("ab", automaton.initialState, false));
+            System.out.println(automaton.testWord("aab", automaton.initialState, false));
+            System.out.println(automaton.testWord("a", automaton.initialState, false));
+            System.out.println(automaton.testWord("bb", automaton.initialState, false));
+            System.out.println(automaton.testWord("abba", automaton.initialState, false));
+            */
 
+            //NFA
+            /*
+            System.out.println(automaton.testWord("ab", automaton.initialState, false));
+            System.out.println(automaton.testWord("aabbb", automaton.initialState, false));
+            System.out.println(automaton.testWord("ac", automaton.initialState, false));
+            System.out.println(automaton.testWord("a", automaton.initialState, false));
+            System.out.println(automaton.testWord("b", automaton.initialState, false));
+            System.out.println(automaton.testWord("abc", automaton.initialState, false));
+            */
+
+            //lambda-NFA
+            /*
+            System.out.println(automaton.testWord("b", automaton.initialState, false));
+            System.out.println(automaton.testWord("bc", automaton.initialState, false));
+            System.out.println(automaton.testWord("aaabccc", automaton.initialState, false));
+            System.out.println(automaton.testWord("c", automaton.initialState, false));
+            System.out.println(automaton.testWord("a", automaton.initialState, false));
+            System.out.println(automaton.testWord("abcb", automaton.initialState, false));
+            */
 
         } catch (Exception e) {
             System.out.println("An error occurred.");
